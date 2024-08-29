79a8c63625775312355a6b4f75eed55ff45f8209
yemal22 <yemalem03@gmail.com>
2024-08-29 23:12:52 +0100

    initialisation

--- ai_blog_generator/ai_blog_generator/settings.py
+++ ai_blog_generator/ai_blog_generator/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    "blog_generator"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "ai_blog_generator.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR, 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
